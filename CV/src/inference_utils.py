cf846ba71e1a46298860886ec86e5a77e727a6db
Yalanskyi Roman <keito.eternal@gmail.com>
2024-12-16 23:52:18 +0200

    additional fixes

--- CV/src/inference_utils.py
+++ CV/src/inference_utils.py
@@ -1,10 +1,12 @@
 import os
+import matplotlib
 import matplotlib.pyplot as plt
 import cv2
 import kornia as K
 import kornia.feature as KF
 import numpy as np
 import torch
+
 from kornia_moons.feature import *
 import torchvision
 from kornia_moons.viz import draw_LAF_matches
@@ -14,10 +16,12 @@ import rasterio
 from PIL import Image
 from lightglue.utils import match_pair
 
+
 def load_torch_image(fname, w, h):
     img = K.image_to_tensor(cv2.resize(cv2.imread(fname), (w, h)), False).float() / 255
     img = K.color.bgr_to_rgb(img)
     return img
+
 
 def resize_torch_crop(tensor_image, w, h):
     tensor_image_resized = F.resize(tensor_image, (h, w))
@@ -27,7 +31,6 @@ def resize_torch_crop(tensor_image, w, h):
 def match_lightglue_crop(tensor_img1, tensor_img2, new_w, new_h, matcher, extractor, old_size, start_w, start_h, device):
     cut_img1 = resize_torch_crop(tensor_img1, new_w, new_h)
     cut_img2 = resize_torch_crop(tensor_img2, new_w, new_h)
-
 
     if torch.cuda.is_available():
         cut_img1 = cut_img1.cuda(device).float()
@@ -74,11 +77,68 @@ def split_image(image, n, save_crops=False, saving_path=None):
 
     return cropped_images
 
+
 def save_image(image, path):
     image_pil = F.to_pil_image(image)
     image_pil.save(path)
 
-def lightglue_matcher(path_img_1, path_img_2, matcher, extractor, w, h, n_pair, crp_w, crp_h, device):
+
+def plot_matches(kpts0, kpts1, color=None, lw=1.5, ps=4, a=1.0, labels=None, axes=None):
+    """Plot matches for a pair of existing images.
+    Args:
+        kpts0, kpts1: corresponding keypoints of size (N, 2).
+        color: color of each match, string or RGB tuple. Random if not given.
+        lw: width of the lines.
+        ps: size of the end points (no endpoint if ps=0)
+        indices: indices of the images to draw the matches on.
+        a: alpha opacity of the match lines.
+    """
+    fig = plt.gcf()
+    if axes is None:
+        ax = fig.axes
+        ax0, ax1 = ax[0], ax[1]
+    else:
+        ax0, ax1 = axes
+    if isinstance(kpts0, torch.Tensor):
+        kpts0 = kpts0.cpu().numpy()
+    if isinstance(kpts1, torch.Tensor):
+        kpts1 = kpts1.cpu().numpy()
+    assert len(kpts0) == len(kpts1)
+    if color is None:
+        color = matplotlib.cm.hsv(np.random.rand(len(kpts0))).tolist()
+    elif len(color) > 0 and not isinstance(color[0], (tuple, list)):
+        color = [color] * len(kpts0)
+
+    if lw > 0:
+        for i in range(len(kpts0)):
+            line = matplotlib.patches.ConnectionPatch(
+                xyA=(kpts0[i, 0], kpts0[i, 1]),
+                xyB=(kpts1[i, 0], kpts1[i, 1]),
+                coordsA=ax0.transData,
+                coordsB=ax1.transData,
+                axesA=ax0,
+                axesB=ax1,
+                zorder=1,
+                color=color[i],
+                linewidth=lw,
+                clip_on=True,
+                alpha=a,
+                label=None if labels is None else labels[i],
+                picker=5.0,
+            )
+            line.set_annotation_clip(True)
+            fig.add_artist(line)
+
+    # freeze the axes to prevent the transform to change
+    ax0.autoscale(enable=False)
+    ax1.autoscale(enable=False)
+
+    if ps > 0:
+        ax0.scatter(kpts0[:, 0], kpts0[:, 1], c=color, s=ps)
+        ax1.scatter(kpts1[:, 0], kpts1[:, 1], c=color, s=ps)
+
+
+def lightglue_matcher(path_img_1, path_img_2, matcher, extractor, w, h, n_pair, crp_w, crp_h, device, limit_printing=False):
     img1 = load_torch_image(path_img_1, w, h).to(device)
     img2 = load_torch_image(path_img_2, w, h).to(device)
 
@@ -87,8 +147,10 @@ def lightglue_matcher(path_img_1, path_img_2, matcher, extractor, w, h, n_pair, 
 
     dict_keys = list(crp_1_img.keys())
     m_kpts0, m_kpts1 = [], []
-    num_match_pairs = 0
-    for pair_index in range(0, 6):
+
+    limit = int(n_pair/2)**2 if not limit_printing else limit_printing
+
+    for pair_index in range(limit):
         crp1 = crp_1_img[dict_keys[pair_index]]['image']
         crp2 = crp_2_img[dict_keys[pair_index]]['image']
 
@@ -116,7 +178,7 @@ def lightglue_matcher(path_img_1, path_img_2, matcher, extractor, w, h, n_pair, 
             ax.axis("off")
 
         # Plot the matches
-        viz2d.plot_matches(pair_mkpts0, pair_mkpts1, color="lime", lw=0.2, axes=axes)
+        plot_matches(pair_mkpts0, pair_mkpts1, color="lime", lw=0.2, axes=axes)
         
         # Finalize and show
         plt.show()
@@ -124,8 +186,8 @@ def lightglue_matcher(path_img_1, path_img_2, matcher, extractor, w, h, n_pair, 
 
 
 def match_loftr_crop(tensor_img1, tensor_img2, new_w, new_h, matcher, old_size, start_w, start_h, device):
-    cut_img1 = resize_torch_cutImage(tensor_img1, new_w, new_h)
-    cut_img2 = resize_torch_cutImage(tensor_img2, new_w, new_h)
+    cut_img1 = resize_torch_crop(tensor_img1, new_w, new_h)
+    cut_img2 = resize_torch_crop(tensor_img2, new_w, new_h)
 
     input_dict = {'image0': K.color.rgb_to_grayscale(cut_img1),
                   'image1': K.color.rgb_to_grayscale(cut_img2)}
@@ -156,7 +218,7 @@ def match_loftr_crop(tensor_img1, tensor_img2, new_w, new_h, matcher, old_size, 
             inliers)
 
 
-def loftr_matcher(path_img_1, path_img_2, matcher, w, h, n_pair, crp_w, crp_h, device):
+def loftr_matcher(path_img_1, path_img_2, matcher, w, h, n_pair, crp_w, crp_h, device, limit_printing=False):
     # n_pair
     # Load images
     img1 = load_torch_image(path_img_1, w, h)
@@ -178,7 +240,9 @@ def loftr_matcher(path_img_1, path_img_2, matcher, w, h, n_pair, crp_w, crp_h, d
 
     inliers, mkpts0, mkpts1 = [], [], []
     
-    for pair_index in range(0, 6):
+    limit = int(n_pair/2)**2 if not limit_printing else limit_printing
+
+    for pair_index in range(limit):
         pair_mkpts0, pair_mkpts1, pair_inliers = match_loftr_crop(
             crp_1_img[dict_keys[pair_index]]['image'],
             crp_2_img[dict_keys[pair_index]]['image'],
